99d0e42220f6b259332b934d5666e8bf39570156
Md. Eyasen Arafat <138970479+mde-arafat@users.noreply.github.com>
2024-11-15 02:34:00 +0600

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,4 +1,14 @@
-def main():
+def name():
   name_input = input("Enter your name: ")
   print(f"Hello, {name_input}")
   return name_input
+
+def age():
+  age_input = input("Enter your age: ")
+  
+  if age_input < 15:
+    print(f"You are a kid. Your age: {age_input}")
+  elif 20 >= age_input >=15:
+    print(f"You are a teenager. Your age: {age_input}")
+  else:
+    print(f"You are an adult guy. Your age: {age_input}")
